8a39b6bc280960e4684bc37090da15cd04082e04
unknown <deniszurav2000@gmail.com>
2023-06-19 00:42:34 +0300

    added lazy loading for users,profile,messages and login pages.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {  Route, Routes } from "react-router-dom";
 import './App.css';
-import ContentContainer, { withRouter } from "./components/Content/contentContainer";
+import  { withRouter } from "./components/Content/contentContainer";
 import Help from "./components/help/help";
 import Music from "./components/Music/music";
 import Nav from './components/Navbar/nav_bar';
 import News from "./components/News/news";
 import Settings from "./components/Settings/settings";
-import DialogsContainer from "./components/Dialogs/dialogsContainer";
-import UsersContainer from "./components/Users/usersContainer";
+// import DialogsContainer from "./components/Dialogs/dialogsContainer";
+// import UsersContainer from "./components/Users/usersContainer";
+// import Login from "./components/login/login";
 import HeaderContainer from "./components/Header/headerContainer";
-import Login from "./components/login/login";
 import { connect } from "react-redux";
+import { lazy } from "react";
 import { compose } from "redux";
 import  store  from './redux/redux-store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { initialize } from "./redux/app-reducer";
 import Preloader from "./components/common/preloader/preloader";
+
+const UsersContainer =  lazy(()=> import("./components/Users/usersContainer"));
+const ContentContainer =  lazy(()=> import("./components/Content/contentContainer"));
+const Login =  lazy(()=> import("./components/login/login"));
+const DialogsContainer =  lazy(()=> import('./components/Dialogs/dialogsContainer'));
+
 class App extends React.Component {
   componentDidMount(){
     this.props.initialize()
@@ -34,8 +41,10 @@ render(){
       <Nav />
       {/* friendsData={props.appState.sidebar.friends} */}
       <div className="app_wrap_content _wrapper">
+      
+      <Suspense fallback={<div><Preloader /></div>}>
         <Routes>
-          <Route path="/Dialogs/*" element={<DialogsContainer />} />
+        <Route path="/Dialogs/*" element={<DialogsContainer/>} />
           <Route path="/Profile/:userId?" element={<ContentContainer />} />
           <Route path="/News/*" element={<News />} />
           <Route path="/Music/*" element={<Music />} />
@@ -46,6 +55,7 @@ render(){
 
 
         </Routes>
+        </Suspense>
       </div>
 
     </div>
@@ -57,18 +67,18 @@ const mapStateToProps =(state)=>({
 export default compose (withRouter,connect(mapStateToProps, {initialize})) (App);
 
 
-let AppContainer = (props) => {
-  compose (withRouter,connect(mapStateToProps, {initialize})) (App);
-}
-const TddApp = (props) =>{
-  return (
-  <React.StrictMode>
-    <BrowserRouter>
-    <Provider store = {store}>
-    <AppContainer />
-    </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
-  )
-}
+// let AppContainer = (props) => {
+//   compose (withRouter,connect(mapStateToProps, {initialize})) (App);
+// }
+// const TddApp = (props) =>{
+//   return (
+//   <React.StrictMode>
+//     <BrowserRouter>
+//     <Provider store = {store}>
+//     <AppContainer />
+//     </Provider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+//   )
+// }
 
